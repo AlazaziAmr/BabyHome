460601634262ed3476148a5d9e31457bce63bf11
NohaAl-Wajeeh <n.alwajeeh@unicomg.com>
2022-10-25 11:47:06 +0300

    edit the last front style

--- resources/views/dashboard/layouts/app.blade.php
+++ resources/views/dashboard/layouts/app.blade.php
@@ -40,7 +40,15 @@ div.dataTables_wrapper div.dataTables_filter input{
     text-align: center;
     font-size:20px
 }
+@media (max-width: 1199.98px){
 
+.g-sidenav-show.rtl .sidenav {
+    transform: translateX(0.875rem);
+}
+.g-sidenav-show.g-sidenav-pinned .sidenav {
+    transform: translateX(26rem);
+}
+}
 </style>
     @endif
     <style>
@@ -133,6 +141,9 @@ margin-top:20px
 .timeline:before{
     border-left: none;
 }
+.navbar-main{
+   background: #fff;
+}
 .navbar-vertical .navbar-brand .navbar-brand-img, .navbar-vertical .navbar-brand span{
        background: white;
     padding: 10px;
@@ -156,6 +167,9 @@ margin-top:20px
 }
 .navbar-vertical .navbar-nav .nav-link[data-bs-toggle="collapse"][aria-expanded="true"]:after{
         color: #fff;
+}
+.text-blue{
+    color:#405165
 }
     </style>
 </head>
@@ -182,12 +196,12 @@ margin-top:20px
         <div class="container-fluid py-1 px-3">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                    <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white"
+                    <li class="breadcrumb-item text-sm"><a class="opacity-5 text-blue"
                                                            href="javascript:;">@lang('site.dashboard')</a>
                     </li>
-                    <li class="breadcrumb-item text-sm text-white active" aria-current="page">{{ $data['title'] }}</li>
+                    <li class="breadcrumb-item text-sm text-blue active" aria-current="page">{{ $data['title'] }}</li>
                 </ol>
-                <h6 class="font-weight-bolder text-white mb-0">{{ $data['title'] }}</h6>
+                <h6 class="font-weight-bolder text-blue mb-0">{{ $data['title'] }}</h6>
             </nav>
             <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
                 <div class="ms-md-auto pe-md-3 d-flex align-items-center">
@@ -195,7 +209,7 @@ margin-top:20px
                 </div>
                 <ul class="navbar-nav  justify-content-end">
                     <li class="nav-item dropdown pe-2 d-flex align-items-center">
-                        <a href="javascript:;" class="nav-link text-white p-0" id="dropdownMenuButton"
+                        <a href="javascript:;" class="nav-link text-blue p-0" id="dropdownMenuButton"
                            data-bs-toggle="dropdown" aria-expanded="false">
                             <i class="fa fa-user me-sm-1"></i> {{Auth::user()->name}}
                         </a>
@@ -224,13 +238,13 @@ margin-top:20px
                     </li>
                     {{--                    <li class="nav-item d-flex align-items-center">--}}
                     {{--                        <a href="{{ route('__bh_.profile') }}"--}}
-                    {{--                           class="nav-link text-white font-weight-bold px-0">--}}
+                    {{--                           class="nav-link text-blue font-weight-bold px-0">--}}
                     {{--                            <i class="fa fa-user me-sm-1"></i>--}}
                     {{--                            <span class="d-sm-inline d-none">@lang('site.profile')</span>--}}
                     {{--                        </a>--}}
                     {{--                    </li>--}}
                     <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
-                        <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+                        <a href="javascript:;" class="nav-link text-blue p-0" id="iconNavbarSidenav">
                             <div class="sidenav-toggler-inner">
                                 <i class="sidenav-toggler-line bg-white"></i>
                                 <i class="sidenav-toggler-line bg-white"></i>
@@ -241,20 +255,20 @@ margin-top:20px
 
                     <li class="nav-item px-3 d-flex align-items-center">
                         @if(app()->getLocale() == 'ar')
-                            <a class="nav-link text-white"
+                            <a class="nav-link text-blue"
                                href="{{ LaravelLocalization::getLocalizedURL('en', null, [], true) }}">
                                 English
 
                             </a>
                         @else
-                            <a class="nav-link text-white"
+                            <a class="nav-link text-blue"
                                href="{{ LaravelLocalization::getLocalizedURL('ar', null, [], true) }}">
                                 العربية
                             </a>
                         @endif
                     </li>
                     <li class="nav-item dropdown pe-2 d-flex align-items-center">
-                        <a href="javascript:;" class="nav-link text-white p-0" id="dropdownMenuButton"
+                        <a href="javascript:;" class="nav-link text-blue p-0" id="dropdownMenuButton"
                            data-bs-toggle="dropdown" aria-expanded="false">
                             <i class="ni ni-bell-55 cursor-pointer"></i>
                         </a>
@@ -294,13 +308,12 @@ margin-top:20px
         <footer class="footer pt-3  ">
             <div class="container-fluid">
                 <div class="row align-items-center justify-content-lg-between">
-                    <div class="col-lg-6 mb-lg-0 mb-4">
-                        <div class="copyright text-center text-sm text-muted text-lg-start">
+                    <div class="col-lg-12 mb-lg-0 mb-4">
+                        <div class="copyright text-center text-sm text-muted text-lg-center">
                             @lang('site.copy')
                         </div>
                     </div>
-                    <div class="col-lg-6">
-                    </div>
+
                 </div>
             </div>
         </footer>
