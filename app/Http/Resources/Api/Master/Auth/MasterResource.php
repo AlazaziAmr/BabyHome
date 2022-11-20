cc176cf9bcf1e6cef79fc06b399c45ce6236374a
AmrAlazazi <a.alazazi@unicomg.com>
2022-11-20 11:29:34 +0300

    Remove name from master.

--- app/Http/Resources/Api/Master/Auth/MasterResource.php
+++ app/Http/Resources/Api/Master/Auth/MasterResource.php
@@ -35,7 +35,7 @@ class MasterResource extends JsonResource
         }else{
             return [
                 'id' => $this->id,
-                'name' => $this->name,
+//                'name' => $this->name,
                 'first_name' => $this->first_name,
                 'last_name' => $this->last_name,
                 'gender' => $this->gender,
