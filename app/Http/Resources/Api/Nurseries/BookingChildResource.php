f20ad3437539f662bd45901e280027a2a2023261
sumaiawaleed <a.albrari@unicomg.com>
2022-12-28 03:04:18 +0300

    Edit BookingChildResource

--- app/Http/Resources/Api/Nurseries/BookingChildResource.php
+++ app/Http/Resources/Api/Nurseries/BookingChildResource.php
@@ -15,22 +15,43 @@ class BookingChildResource extends JsonResource
     public function toArray($request)
     {
 
+
         $images = array();
-        if($this->booking['children']->attachmentable()) {
+        $child = array();
+
+        if($this->booking()) {
+
+        foreach($this->booking()->get() as  $kids){
+            foreach($kids->children()->get() as $kid) {
+                $child['child_id']=$kid->id;
+                $child['name']=$kid->name;
+                $child['dateOfBirth']=$kid->date_of_birth;
+                $child['description']=$kid->description;
+                $child['gender']=$kid->gender_id;
+                $images['image'] = env('APP_URL').'/storage/children/'.$kid->attachmentable()->first()->path;
+
+
+
+            }
+
+            }
+            }
+   /*     if($this->booking['children']->attachmentable()) {
                 foreach ($this->booking['children']->attachmentable()->get() as $index=>$image) {
                     $images[$index]['id'] =  $image->id;
                     $images[$index]['image_path'] =  asset('storage/children/' . $image->path);
                 }
-            }
+            }*/
         $data = [
             'booking' => [
             'id' => $this->id,
-            'child_id' => $this->booking->children->id,
+/*            'child_id' => $this->booking->children->id,*/
             'confirmDates' => $this->confirm_date,
-            'name' => $this->booking->children->name,
-            'dateOfBirth' => $this->booking->children->date_of_birth,
-            'description' => $this->booking->children->description,
-            'gender' => $this->booking->children->gender_id,
+/*            'name' => $this->booking->children->name,*/
+/*            'dateOfBirth' => $this->booking->children->date_of_birth,*/
+/*            'description' => $this->booking->children->description,*/
+/*            'gender' => $this->booking->children->gender_id,*/
+                'child' => $child,
                 'image' => $images,
             ],
         ];
