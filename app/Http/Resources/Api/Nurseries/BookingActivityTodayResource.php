4d87bc7a00618778189e17e35b134dd7bf912384
AmrAlazazi <a.alazazi@unicomg.com>
2023-01-01 13:25:17 +0300

    Show today services only

--- app/Http/Resources/Api/Nurseries/BookingActivityTodayResource.php
+++ app/Http/Resources/Api/Nurseries/BookingActivityTodayResource.php
@@ -28,7 +28,8 @@ class BookingActivityTodayResource extends JsonResource
             ->where('status',2)
             ->pluck('booking_id')->toArray();
 
-        foreach($this->booking_service()->whereIn('booking_id',$ConfirmedBooking)->get() as $k => $kids){
+        $booking_service = $this->booking_service()->whereIn('booking_id',$ConfirmedBooking)->get();
+        foreach($booking_service as $k => $kids){
             $details[$k]['id'] = $kids->id;
             $details[$k]['service_id'] = $kids->service_id;
             $details[$k]['booking_id'] = $kids->booking_id;
@@ -39,22 +40,23 @@ class BookingActivityTodayResource extends JsonResource
             foreach($kids->childrens()->get() as $kid) {
                 $children[$k]['id'] = $kid->id;
                 $children[$k]['name'] = $kid->name;
-                $children[$k]['image'] = config('app.url').'/storage/children/'.$kid->attachmentable()->first()->path;
+                $children[$k]['image'] = url('/').'/storage/children/'.$kid->attachmentable()->first()->path;
             }
         }
-
-        $data = [
-            'activity' => [
-                'id'=>$this->id,
-                'booking_service_details' => $details,
-                'name'=>$this->name,
-                'description'=>$this->description,
-                'is_paid'=>$this->is_paid,
-                'is_active'=>$this->is_active,
-                'image' => $this->mainAttachment,
-                'child' => $children,
-            ],
-        ];
+        if ($booking_service->count() > 0) {
+            $data = [
+                'activity' => [
+                    'id' => $this->id,
+                    'booking_service_details' => $details,
+                    'name' => $this->name,
+                    'description' => $this->description,
+                    'is_paid' => $this->is_paid,
+                    'is_active' => $this->is_active,
+                    'image' => $this->mainAttachment,
+                    'child' => $children,
+                ],
+            ];
+        }
 
        /* $data['child']=[
             $this->children->service_id->id
